64747e7d8e9f948e8f07e100c67d14d1f75e43b3
Rukmin Tharanga <rukmintharangaofficial@gmail.com>
2025-08-28 08:59:18 +0530

    add fav icon and updated the nav bar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,11 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/">🎬 Movie App</Link>
+                <Link to="/react_Movie_Website">🎬 Movie App</Link>
             </div>
             <div className="navbar-links">
                 {/* Navigation links for Home and Favorites pages */}
-                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/react_Movie_Website" className="nav-link">Home</Link>
                 <Link to="/favorites" className="nav-link">Favorites</Link>
             </div>
         </nav>
